035c75a901009dd42f1be9b68aaf8429cf711d2a
Elvall <93067575+Elvall@users.noreply.github.com>
2023-07-12 08:52:11 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.eivggif.mongodb.net',
+  DB_USER: 'ngovuong450',
+  DB_PASS: 'Vuong450',
+  DB_DATABASE: 'Shoppingonline',
+  EMAIL_USER: 'ngovuong450@gmail.com', 
+  EMAIL_PASS: 'fyqjcykidbczxlnf',
+  JWT_SECRET: '1234',
+  JWT_EXPIRES: '36000000', // in milliseconds
+};
+module.exports = MyConstants;
